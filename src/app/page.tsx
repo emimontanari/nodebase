6e4460805f2c8d5cf6b7058d5ab8a166fe55b6e7
emimontanari <emimontanari@icloud.com>
2026-01-13 15:51:12 -0300

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany();
   return (
     <div className="text-red-500">
       <h1>Hello World!</h1>
       <Button>Click me</Button>
+
+      {JSON.stringify(users)}
     </div>
   );
 };
