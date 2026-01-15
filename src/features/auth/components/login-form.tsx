ac9dfaadf4a563f8f9f531c7db01e732cbd764ba
emimontanari <emimontanari@icloud.com>
2026-01-15 14:15:07 -0300

    05: theme styling

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -92,6 +92,12 @@ export function LoginForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="/logos/github.svg"
+                      width={20}
+                      height={20}
+                      alt="Github"
+                    />
                     Continue with Github
                   </Button>
                   <Button
@@ -100,6 +106,12 @@ export function LoginForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="/logos/google.svg"
+                      width={20}
+                      height={20}
+                      alt="Github"
+                    />
                     Continue with Google
                   </Button>
                 </div>
