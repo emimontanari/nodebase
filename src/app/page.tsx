eb990568589c551a1624ab24016aebe160d950ee
emimontanari <emimontanari@icloud.com>
2026-01-21 15:39:09 -0300

    06: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-export default async function Page() {
-  await requireAuth();
-
-  const data = await caller.getUsers();
+export default function Page() {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const queryClient = useQueryClient();
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       Protectec server componet
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
     </div>
   );
 }
